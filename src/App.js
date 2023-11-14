a67213af56334466bf542b427d142fa5a67ecbf8
karina77478 <bordakari9@gmail.com>
2023-11-14 18:18:23 -0500

    examen2

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import C06componente from './component/C06componente';
+import C05contador from './component/C05contador';
+import C04doblecontador from './component/C04doblecontador';
+import C03variable from './component/C03variable';
+import C02operador from './component/C02operador';
+import C01matriz from './component/C01matriz';
+
+function App() {
+  return (
+    <div>
+      •Nombre de la institucion
+      <h3>HONORIO DELGADO ESPINOZA"</h3>
+      • Nombre del estudiante
+      <h3>Valdivia Borda Luz Karina</h3>
+      • Turno
+      <h3>NOCHE VI SEMESTRE</h3>
+      
+      
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/C06componente.jsx
@@ -0,0 +1,12 @@
+import React from 'react'       //rafce
+
+const C06componente = () => {
+  return (
+    
+    <div>
+      Hola mundo desde React;
+    </div>
+  )
+}
+
+export default C06componente;
